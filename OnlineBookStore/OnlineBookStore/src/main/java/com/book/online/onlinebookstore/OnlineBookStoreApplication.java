3787f2e97972f5a46874e8d403f32f994fbd42d3
ganesh <ganesh2223@gmail.com>
2020-05-04 19:47:06 +0530

    second feature completed

--- OnlineBookStore/OnlineBookStore/src/main/java/com/book/online/onlinebookstore/OnlineBookStoreApplication.java
+++ OnlineBookStore/OnlineBookStore/src/main/java/com/book/online/onlinebookstore/OnlineBookStoreApplication.java
@@ -3,6 +3,7 @@ package com.book.online.onlinebookstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class OnlineBookStoreApplication {
 
